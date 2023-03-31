c54b5242bdb3d30ed076a4ee10843967a1f88426
Moses Wejuli <mwejuli@companieshouse.gov.uk>
2023-03-31 12:58:12 +0100

    feature: add error pages and upgrade mocha version

--- src/router.dispatch.js
+++ src/router.dispatch.js
@@ -4,4 +4,7 @@ module.exports = function (app) {
     app.use("/", require("./routers/indexRouter"));
     app.use("/user", require("./routers/userRouter"));
     app.use("/company", require("./routers/companyRouter"));
+    app.use("*", (req, res) => {
+        res.status(404).render("partials/error_400");
+    });
 };
